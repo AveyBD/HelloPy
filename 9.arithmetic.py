f2a33f612057c5d2a4243b321d4467dc60ee3044
aveybd <debashishdab@gmail.com>
2019-02-22 18:01:45 +0600

    Math

--- 9.arithmetic.py
+++ 9.arithmetic.py
@@ -4,3 +4,15 @@ print(10+5)
 print(10-5)
 print(10/5)
 print(10*5)
+print(7 % 3)
+
+#We can print without the decimal value. Just we have to add another forward slash
+
+print(7 // 2)
+
+#Power of a  int
+print(10 ** 7 )
+#augmented variable
+x = 5
+x += 3
+print(x)
